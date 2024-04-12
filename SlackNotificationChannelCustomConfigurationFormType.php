a8a4cfed4fb28c2fa7575738485fd2605b5d5744
Dominik Płachta <dplachta03@gmail.com>
2024-04-12 17:04:00 +0200

    Add validation constraints

--- SlackNotificationChannelCustomConfigurationFormType.php
+++ SlackNotificationChannelCustomConfigurationFormType.php
@@ -7,14 +7,22 @@ namespace EightLines\SyliusNotificationPlugin\NotificationChannel\Symfony;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 
 final class SlackNotificationChannelCustomConfigurationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('recipient', TextType::class, [
-            'label' => 'eightlines_sylius_notification_plugin.form.notification_channel.recipient',
+            'label' => 'eightlines_sylius_notification_plugin.ui.recipient',
             'required' => false,
+            'constraints' => [
+                new Length([
+                    'max' => 250,
+                    'maxMessage' => 'eightlines_sylius_notification_plugin.notification.action.custom.recipient.max_length',
+                    'groups' => ['sylius'],
+                ]),
+            ],
         ]);
     }
 }
